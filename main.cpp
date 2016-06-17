d8249476404e0656efbd9cc3838f2d2f4b67ab4f
Kevin Wu <kevinwu_mtk@yahoo.com>
2016-06-17 12:35:40 +0200

    Added the main C++ file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ARTowerDefense
+//
+//  Created by Kevin Wu on 17/06/16.
+//  Copyright © 2016 Kevin Wu. All rights reserved.
+//
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
