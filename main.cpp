a1e863011dcf4418259648a288fa94139131fc53
Markus Müller <sukram.mueller@gmail.com>
2016-07-08 18:05:55 +0200

    Wrong corners for snowman display, but sufficient for testing

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include "ComputerVision/ComputerVision.hpp"
 #include <iomanip>
 
+#include "DrawUnits.h"
+
 #define GLFW_INCLUDE_GLU // add support for GLU with GLFW
 #include <GLFW/glfw3.h> /// this also includes other openGL headers
 
@@ -24,6 +26,12 @@ using namespace std;
 const int EXIT = -1;
 const int FREE_FIELD = 0;
 
+//camera settings
+const int camera_width = 640;
+const int camera_height = 480;
+const int virtual_camera_angle = 30;
+unsigned char bkgnd[camera_width*camera_height * 3];
+
 using namespace std::chrono;
 
 std::vector<int> getNextMonsterStep(std::vector<int> monster_current_gamefield_position, int gamefield_matrix[][FIELD_SIZE_COLS])
@@ -35,6 +43,149 @@ std::vector<int> getNextMonsterStep(std::vector<int> monster_current_gamefield_p
         monster_current_gamefield_position.at(1) + 1
     });
 }
+
+/* program & OpenGL initialization */
+void initGL(int argc, char *argv[])
+{
+	// initialize the GL library
+
+	// Added in Exercise 8 - End *****************************************************************
+	// pixel storage/packing stuff
+	glPixelStorei(GL_PACK_ALIGNMENT, 1); // for glReadPixels?
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // for glTexImage2D?
+	glPixelZoom(1.0, -1.0);
+	// Added in Exercise 8 - End *****************************************************************
+
+	// enable and set colors
+	glEnable(GL_COLOR_MATERIAL);
+	glClearColor(0, 0, 0, 1.0);
+
+	// enable and set depth parameters
+	glEnable(GL_DEPTH_TEST);
+	glClearDepth(1.0);
+
+	// light parameters
+	GLfloat light_pos[] = { 1.0, 1.0, 1.0, 0.0 };
+	GLfloat light_amb[] = { 0.2, 0.2, 0.2, 1.0 };
+	GLfloat light_dif[] = { 0.7, 0.7, 0.7, 1.0 };
+
+	// enable lighting
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_amb);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_dif);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+
+}
+
+void display(GLFWwindow* window, const cv::Mat &img_bgr, float resultMatrix[16])
+{
+	// Added in Exercise 8 - Start *****************************************************************
+	memcpy(bkgnd, img_bgr.data, sizeof(bkgnd));
+	// Added in Exercise 8 - End *****************************************************************
+
+	int width0, height0;
+	glfwGetFramebufferSize(window, &width0, &height0);
+	//	reshape(window, width, height);
+
+	// clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	// Added in Exercise 8 - Start *****************************************************************
+	// draw background image
+	glDisable(GL_DEPTH_TEST);
+
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+	gluOrtho2D(0.0, camera_width, 0.0, camera_height);
+
+	glRasterPos2i(0, camera_height - 1);
+	glDrawPixels(camera_width, camera_height, GL_BGR_EXT, GL_UNSIGNED_BYTE, bkgnd);
+
+	glPopMatrix();
+
+	glEnable(GL_DEPTH_TEST);
+
+	// Added in Exercise 8 - End *****************************************************************
+
+	// move to marker-position
+	glMatrixMode(GL_MODELVIEW);
+
+	float resultTransposedMatrix[16];
+	for (int x = 0; x<4; ++x)
+	{
+		for (int y = 0; y<4; ++y)
+		{
+			resultTransposedMatrix[x * 4 + y] = resultMatrix[y * 4 + x];
+		}
+	}
+
+
+	// CHECK OPENGL COORDINATE SYSTEM: http://www.cocos2d-x.org/attachments/download/1563
+
+
+
+
+	//glLoadTransposeMatrixf( resultMatrix );
+	glLoadMatrixf(resultTransposedMatrix);
+	glRotatef(-90, 1, 0, 0);
+	glScalef(0.03, 0.03, 0.03);
+
+	glTranslatef(0, -5, 0);
+
+
+	// draw 3 white spheres
+	glColor4f(1.0, 1.0, 1.0, 1.0);
+	drawSphere(0.8, 10, 10);
+	glTranslatef(0.0, 0.8, 0.0);
+	drawSphere(0.6, 10, 10);
+	glTranslatef(0.0, 0.6, 0.0);
+	drawSphere(0.4, 10, 10);
+
+	// draw the eyes
+	glPushMatrix();
+	glColor4f(0.0, 0.0, 0.0, 1.0);
+	glTranslatef(0.2, 0.2, 0.2);
+	drawSphere(0.066, 10, 10);
+	glTranslatef(0, 0, -0.4);
+	drawSphere(0.066, 10, 10);
+	glPopMatrix();
+
+	// draw a nose
+	glColor4f(1.0, 0.5, 0.0, 1.0);
+	glTranslatef(0.3, 0.0, 0.0);
+	glRotatef(90, 0, 1, 0);
+	drawCone(0.1, 0.3, 10, 10);
+
+
+}
+
+void reshape(GLFWwindow* window, int width, int height) {
+
+	// set a whole-window viewport
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+
+	// create a perspective projection matrix
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	// Note: Just setting the Perspective is an easy hack. In fact, the camera should be calibrated.
+	// With such a calibration we would get the projection matrix. This matrix could then be loaded 
+	// to GL_PROJECTION.
+	// If you are using another camera (which you'll do in most cases), you'll have to adjust the FOV
+	// value. How? Fiddle around: Move Marker to edge of display and check if you have to increase or 
+	// decrease.
+	gluPerspective(virtual_camera_angle, ((GLfloat)width / (GLfloat)height), 0.01, 100);
+
+	// invalidate display
+	//glutPostRedisplay();
+}
+
 
 int main(int ac, char** av)
 {
@@ -57,6 +208,38 @@ int main(int ac, char** av)
     }
     
     namedWindow("frame",1);
+
+
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+
+	// initialize the window system
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(camera_width, camera_height, "Exercise 8 - Combine", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	// Set callback functions for GLFW
+	glfwSetFramebufferSizeCallback(window, reshape);
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	int window_width, window_height;
+	glfwGetFramebufferSize(window, &window_width, &window_height);
+	reshape(window, window_width, window_height);
+
+	glViewport(0, 0, window_width, window_height);
+
+	// initialize the GL library
+	initGL(ac, av);
     
     while (true) {
         Mat frame;
@@ -145,7 +328,20 @@ int main(int ac, char** av)
         std::cout << i << "\tMonster position: \tx: " << monster_current_gamefield_position.at(1) << "\ty: " << monster_current_gamefield_position.at(0) << "\n";
         i++;
         
-        imshow("frame", frame);
+        // imshow("frame", frame);
+		float resultMatrix[16];
+		const double kMarkerSize = 0.048;
+		estimateSquarePose(resultMatrix, (cv::Point2f*)gameBoardCorners, kMarkerSize); // TODO: shouldn't be gameBoardCorners, but the corners around the figure
+
+		/* Render here */
+		display(window, frame, resultMatrix);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+
         char keypress;
         keypress = waitKey(30);
         if(keypress==27) break;
@@ -155,6 +351,7 @@ int main(int ac, char** av)
     }
     
     //release heap (program exit)
+	glfwTerminate();
     cvReleaseMemStorage(&memStorage);
     
 }
